03ced57940ce14f94fcfb46c3f4e92a77583063e
suer <sue@nagoya-u.jp>
2011-05-04 01:59:06 +0900

    available commands check

--- src/main/scala/org/codefirst/shimbashishelf/ShimbashiShelf.scala
+++ src/main/scala/org/codefirst/shimbashishelf/ShimbashiShelf.scala
@@ -1,16 +1,27 @@
 package org.codefirst.shimbashishelf
+
 import org.apache.lucene.document.Document
 import java.io.File
 
 object ShimbashiShelf {
   val INDEX_PATH : String  = "index"
-
+  val commands : Array[String] = Array("index", "search")
   def main(args : Array[String]) {
     if (args.length == 0) { 
       println("no arguments found")
+      println("available commands:")
+      commands.foreach((command) => println("   " + command))
+
       System.exit(0)
     }
-    
+
+    if (!commands.contains(args(0))) {
+      println("unknown command")
+      println("available commands:")
+      commands.foreach((command) => println("   " + command))
+      System.exit(0)
+    }
+
     if (args(0).equals("search")) { 
       if (args.length < 2) { 
         println("usage: search <word>")
