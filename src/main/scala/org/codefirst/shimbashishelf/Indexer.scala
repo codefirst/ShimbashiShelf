69ef450e458a95fd511e2ae034f9fd8c832b4ea6
mzp <mzpppp@gmail.com>
2011-05-04 16:38:16 +0900

    use Loan pattern refs #567

--- src/main/scala/org/codefirst/shimbashishelf/Indexer.scala
+++ src/main/scala/org/codefirst/shimbashishelf/Indexer.scala
@@ -14,37 +14,38 @@ import org.apache.lucene.index.{IndexWriterConfig, IndexWriter}
 import org.apache.lucene.analysis.SimpleAnalyzer
 import org.apache.lucene.index.Term
 
+object SLucene {
+  implicit def tuple2term(x : (String, String)) : Term =
+    new Term(x._1, x._2)
+
+  implicit def tuple2field(x : (String, String, Field.Store, Field.Index)) : Field =
+    new Field(x._1, x._2, x._3, x._4)
+}
+
 object Indexer {
+  import SLucene._
+
+
+  def using[A <: { def close() : Unit }, B](resource : A)(body : A => B) : Option[B] =
+    try     { Some(body(resource)) }
+    catch   { case _ => None }
+    finally { resource.close() }
+
 
   def index(path : String, text : String) : Boolean = {
-    if (text == null) {
-      return true
-    }
-
-    var writer : IndexWriter = null
-    var dir : Directory = null
-    try {
-      dir = FSDirectory.open(new File(INDEX_PATH))
-      writer = new IndexWriter(dir, new IndexWriterConfig(Version.LUCENE_31, new CJKAnalyzer(Version.LUCENE_31)))
-
-      writer.deleteDocuments(new Term("path", path))
-
-      val doc : Document = new Document()
-
-      val pathField : Field = new Field("path", path, Store.YES, Index.NOT_ANALYZED)
-      val contentField : Field = new Field("content", text, Store.YES, Index.ANALYZED)
-
-      doc.add(pathField)
-      doc.add(contentField)
-      writer.addDocument(doc)
-    } catch {
-      case e:IOException => return false
-    } finally {
-      if (writer != null) {
-        writer.close()
-      }
-    }
-    return true
+    val config = new IndexWriterConfig(Version.LUCENE_31, new CJKAnalyzer(Version.LUCENE_31))
+    using(FSDirectory.open(new File(INDEX_PATH))) { case dir =>
+      using(new IndexWriter(dir, config)){
+	case writer => {
+	  writer.deleteDocuments(("path", path))
+	  val doc : Document = new Document()
+	  doc.add(("path", path, Store.YES, Index.NOT_ANALYZED))
+	  doc.add(("content", text, Store.YES, Index.ANALYZED))
+	  writer.addDocument(doc)
+	}}} match {
+	  case Some(_) => true
+	  case None    => false
+	}
   }
 
   def index(file : File) : Boolean = {
