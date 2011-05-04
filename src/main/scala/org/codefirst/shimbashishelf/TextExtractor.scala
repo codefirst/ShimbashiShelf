afdf3f6ba96508a9f32ad43a3f56ac1721de19b9
mzp <mzpppp@gmail.com>
2011-05-04 16:03:14 +0900

    refactoring TextExtractor refs #556

--- src/main/scala/org/codefirst/shimbashishelf/TextExtractor.scala
+++ src/main/scala/org/codefirst/shimbashishelf/TextExtractor.scala
@@ -1,4 +1,5 @@
 package org.codefirst.shimbashishelf
+import scala.io.Source
 import java.io._
 import org.pdfbox.util._
 import org.pdfbox.pdfparser._
@@ -12,41 +13,67 @@ import org.apache.poi.xslf.extractor._
 import org.apache.poi.xssf.extractor._
 import org.apache.poi.extractor._
 
-object TextExtractor {
-    def extract(fileName : String) : String = {
-        if (fileName == null) {
-            return null;
-        }
-        if (fileName.lastIndexOf(".") > 0) {
-            var extension : String = fileName.substring(fileName.lastIndexOf(".") + 1)
-            if (extension.equals("pdf")) {
-              return extractPDF(fileName)
-            } else if (isOfficeFile(extension)) {
-              return extractOfficeDocument(fileName)
-            }
-        }
-        return null
-    }
-
-    def isOfficeFile(extension : String) : Boolean = List("doc", "docx", "ppt", "pptx", "xls", "xlsx").contains(extension)
-
-    private def extractPDF(fileName : String) : String = {
-        val in : InputStream = new FileInputStream(fileName)
-        val pdfParser : PDFParser = new PDFParser(in)
-        pdfParser.parse()
-        return new PDFTextStripper().getText(pdfParser.getPDDocument())
-    }
-
-    private def extractOfficeDocument(fileName : String) : String = {
-        val in : InputStream = new FileInputStream(fileName)
-        return ExtractorFactory.createExtractor(in).getText()
-    }
-
-    def main(args : Array[String]) {
-      for (fileName <- args) {
-        println(fileName)
-        println(extract(fileName))
-      }
-    }
+trait Extractor{
+  def extract(fileName : String) : Option[String]
 }
 
+object FileNameUtil {
+  def getExtension(path : String) : String = {
+    val i =path.lastIndexOf(".")
+    if (i > 0)
+      path.substring(i + 1)
+    else
+      ""
+  }
+}
+
+object PdfExtractor extends Extractor {
+  def extract(fileName : String) = {
+    if(FileNameUtil.getExtension(fileName) == "pdf") {
+      val in : InputStream = new FileInputStream(fileName)
+      val pdfParser : PDFParser = new PDFParser(in)
+      pdfParser.parse()
+      Some(new PDFTextStripper().getText(pdfParser.getPDDocument()))
+    } else
+      None
+  }
+}
+
+object OfficeExtractor extends Extractor {
+  val extensions = List("doc", "docx", "ppt", "pptx", "xls", "xlsx")
+
+  def extract(fileName : String) = {
+    val ext = FileNameUtil.getExtension(fileName)
+    if(extensions contains ext){
+      val in : InputStream = new FileInputStream(fileName)
+      Some(ExtractorFactory.createExtractor(in).getText())
+    } else
+      None
+  }
+}
+
+object TextExtractor {
+  private val Extractors : List[Extractor] = List(PdfExtractor,
+						  OfficeExtractor)
+
+  private def any[A,B](xs : List[A])(f : A => Option[B]) : Option[B] = {
+    xs match {
+      case List() => None
+      case x::xs  =>
+	f(x) match {
+	  case x @ Some(_) => x
+	  case None => any(xs)(f)
+	}
+    }
+  }
+
+  def extract(fileName : String) : String =
+    any(Extractors){ e => e.extract(fileName) }.getOrElse("")
+
+  def main(args : Array[String]) {
+    for (fileName <- args) {
+      println(fileName)
+      println(extract(fileName))
+    }
+  }
+}
