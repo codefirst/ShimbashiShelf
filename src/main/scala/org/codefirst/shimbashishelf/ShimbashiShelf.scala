13601f253627d7d0086b62142760f56b154c4b56
suer <sue@nagoya-u.jp>
2011-05-07 01:02:51 +0900

    全更新履歴の取得 refs #577

--- src/main/scala/org/codefirst/shimbashishelf/ShimbashiShelf.scala
+++ src/main/scala/org/codefirst/shimbashishelf/ShimbashiShelf.scala
@@ -2,72 +2,90 @@ package org.codefirst.shimbashishelf
 
 import org.apache.lucene.document.Document
 import java.io.File
+import java.util.Calendar
+import java.util.Date
+import java.text.SimpleDateFormat
 
 object ShimbashiShelf {
   val INDEX_PATH : String  = "index"
-  val commands : Array[String] = Array("index", "search", "index-all", "search-by-path", "commit")
+  val commands : Array[String] = Array("index", "search", "index-all", "search-by-path", "commit", "history")
   def main(args : Array[String]) {
     args.toList match {
       case List() => {
-	println("no arguments found")
-	println("available commands:")
-	commands.foreach((command) => println("   " + command))
+	    println("no arguments found")
+	    println("available commands:")
+	    commands.foreach((command) => println("   " + command))
       }
       case "search"::args =>
-	if(args.length < 1) {
+	    if(args.length < 1) {
           println("usage: search <word>")
-	} else {
-	  val documents : Array[Document] = Searcher.search(args(0), "content")
-	  println(documents.length + " documents found:")
-	  documents.foreach ((doc) => println(doc.getField("path").stringValue()))
-	}
+	    } else {
+	      val documents : Array[Document] = Searcher.search(args(0), "content")
+	      println(documents.length + " documents found:")
+	      documents.foreach ((doc) => println(doc.getField("path").stringValue()))
+	    }
       case "search-by-path"::args =>
-	if (args.length < 1) {
+	    if (args.length < 1) {
           println("usage: search-by-path <path>")
-	} else try {
+	    } else try {
           val document : Document = Searcher.searchByPath(args(0))
           println("found:")
           println(document.getField("path").stringValue())
-	} catch {
+	    } catch {
           case e : Exception => e.printStackTrace()
-	}
+	    }
       case "index"::args =>
-	if (args.length < 1) {
+	    if (args.length < 1) {
           println("usage: index <filepath>")
-	} else {
-	  val file : File = new File(args(0))
-	  if (Indexer.index(file)) {
+	    } else {
+	      val file : File = new File(args(0))
+	      if (Indexer.index(file)) {
             println("index successful")
-	  } else {
+	      } else {
             println("index failed")
-	  }
-	}
+	      }
+	    }
       case "index-all"::args =>
-	if (args.length < 1) {
+	    if (args.length < 1) {
           println("usage: index-all <directory-path>")
-	} else {
-	  val dir : File = new File(args(0))
-	  if (Indexer.indexRecursively(dir)) {
+	    } else {
+	      val dir : File = new File(args(0))
+	      if (Indexer.indexRecursively(dir)) {
             println("index successful")
-	  } else {
+	      } else {
             println("index failed")
-	  }
-	}
+	      }
+	    }
       case "commit"::args =>
-	if (args.length < 1) {
+	    if (args.length < 1) {
           println("usage: commit <filepath>")
-	} else {
-	  val vc : VersionControl = new VersionControl(new File("files"))
-	  if (vc.commit(new File(args(0)))) {
+	    } else {
+	      val vc : VersionControl = new VersionControl(new File("files"))
+	      if (vc.commit(new File(args(0)))) {
             println("commit successful")
-	  } else {
+	      } else {
             println("commit failure")
-	  }
-	}
+	      }
+	    }
+      case "history"::args =>
+	    val vc : VersionControl = new VersionControl(new File("files"))
+        val commits : List[Commit] = vc.commitList()
+
+        val format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+        val cal = Calendar.getInstance()
+
+        for (commit <- commits) { 
+          println("hash: " + commit.getHash())
+          println("date: " + format.format(commit.getDate()))
+          println("modified: ")
+          commit.getFiles().foreach { file => println("   " + file) }
+          println()
+        }
+
       case _ => {
-	println("unknown command")
-	println("available commands:")
-	commands.foreach((command) => println("   " + command))
+	    println("unknown command")
+	    println("available commands:")
+	    commands.foreach((command) => println("   " + command))
       }
     }
   }
--- src/main/scala/org/codefirst/shimbashishelf/VersionControl.scala
+++ src/main/scala/org/codefirst/shimbashishelf/VersionControl.scala
@@ -2,11 +2,25 @@ package org.codefirst.shimbashishelf
 
 import org.eclipse.jgit.api._
 import org.eclipse.jgit.lib._
+import org.eclipse.jgit.revwalk._
+import org.eclipse.jgit.treewalk._
+import org.eclipse.jgit.treewalk.filter._
+
 import java.io.File
-import java.util.Calendar
+import java.util._
 import java.text.SimpleDateFormat
 
+import collection.JavaConversions._
+import scala.collection.mutable._
+
+class Commit(hash : String, date : Date, files : scala.collection.immutable.List[String]) {
+  def getHash() = hash
+  def getDate() = date
+  def getFiles() = files
+}
+
 class VersionControl(repositoryDir : File) {
+
   val repository : Repository = new RepositoryBuilder().setGitDir(new File(repositoryDir.getAbsolutePath() + File.separatorChar + Constants.DOT_GIT)).readEnvironment().findGitDir().build()
 
   def commit(file : File) : Boolean = {
@@ -29,4 +43,29 @@ class VersionControl(repositoryDir : File) {
     return true
   }
 
+  def commitList() : scala.collection.immutable.List[Commit] = {
+    val git : Git = new Git(repository)
+
+    val tw = new NameConflictTreeWalk(repository.newObjectReader())
+    tw.setRecursive(true)
+
+    var commits : ListBuffer[Commit] = new ListBuffer()
+    for (commit <- git.log().call()) { 
+      tw.reset(commit.getTree())
+      if (commit.getParentCount() >= 1) { 
+        tw.setFilter(TreeFilter.ANY_DIFF)
+        tw.addTree(commit.getParent(0).getTree())
+      } else { 
+        tw.setFilter(TreeFilter.ALL)
+      }
+      var files : ListBuffer[String] = new ListBuffer()
+      while (tw.next()) { 
+        files.add(tw.getPathString())
+      }
+      val cal = Calendar.getInstance()
+      cal.setTimeInMillis(commit.getCommitTime().asInstanceOf[Long] * 1000)
+      commits.add(new Commit(commit.getName(),  cal.getTime(), files.toList))
+    }
+    commits.toList
+  }
 }
