f53761834eefb30540dde3e097669349e3cfec99
mzp <mzpppp@gmail.com>
2011-05-21 13:24:42 +0900

    SCalendarの単体テスト追加 refs #609

--- src/test/scala/org/codefirst/shimbashishelf/util/SCalendarSpec.scala
+++ src/test/scala/org/codefirst/shimbashishelf/util/SCalendarSpec.scala
@@ -3,39 +3,31 @@ import org.scalatest.Spec
 import org.scalatest.matchers.MustMatchers
 import org.scalatest.matchers.ShouldMatchers
 import java.util.Calendar
-import org.codefirst.shimbashishelf.util.SCalendar
 
 class SCalendarSpec extends Spec with ShouldMatchers {
-  describe("2011/4/14") {
-    it("get start date of month") {
-      val cal = Calendar.getInstance()
-      cal.set(Calendar.YEAR, 2011)
-      cal.set(Calendar.MONTH, Calendar.APRIL)
-      cal.set(Calendar.DATE, 14 )
-      val scalendar = new SCalendar(cal.getTime())
+  describe("日付の情報取得") {
+    val scalendar = SCalendar(2011,4,14)
+
+    it("最初の日付の取得") {
       new SCalendar(scalendar.startDayOfMonth()).get(Calendar.DATE) should be (1)
     }
-    it("get end date of month") {
-      val cal = Calendar.getInstance()
-      cal.set(Calendar.YEAR, 2011)
-      cal.set(Calendar.MONTH, Calendar.APRIL)
-      cal.set(Calendar.DATE, 14 )
-      val scalendar = new SCalendar(cal.getTime())
+
+    it("最後の日付の取得") {
       new SCalendar(scalendar.endDayOfMonth()).get(Calendar.DATE) should be (30)
+    }
+
+    it("文字列化") {
+      scalendar.toString should be("2011/4/14 0:0:0 0 AM")
     }
   }
 
-  describe("2011/4/1 - 2011/4/30") { 
-    it("iterate through a month") { 
-      val cal = Calendar.getInstance()
-      cal.set(Calendar.YEAR, 2011)
-      cal.set(Calendar.MONTH, Calendar.APRIL)
+  describe("イテレータ") {
+    it("開始日・終了日指定") {
+      val cal = SCalendar(2011,4, 1)
       val startDate = new SCalendar(cal.getTime()).startDayOfMonth()
       val endDate = new SCalendar(cal.getTime()).endDayOfMonth()
-      
-      new SCalendar(startDate).iterator(endDate).foreach { date =>        
-        println(new SCalendar(date))
-      }
+
+      new SCalendar(startDate).iterator(endDate).length should be (30)
     }
   }
 }
