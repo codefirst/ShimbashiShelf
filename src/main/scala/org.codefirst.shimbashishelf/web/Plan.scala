335b7c72a9677915ca598f81628b5db56d7e8f0f
mzp <mzpppp@gmail.com>
2011-06-20 23:53:02 +0900

    個別表示ページをScamlに移植した refs #658

--- src/main/scala/org.codefirst.shimbashishelf/web/Plan.scala
+++ src/main/scala/org.codefirst.shimbashishelf/web/Plan.scala
@@ -4,6 +4,8 @@ import unfiltered.request._
 import unfiltered.response._
 import unfiltered.scalate._
 
+import org.codefirst.shimbashishelf.filesystem._
+
 class Plan extends unfiltered.filter.Planify({
   case Path(Seg("static" :: _)) =>
     Pass
@@ -17,6 +19,21 @@ class Plan extends unfiltered.filter.Planify({
     Calendar(req)
   case req@Path(Seg("show"::id::_)) =>
     Show(req, id)
+  case req@Path(Seg("download"::id::_)) =>
+    val response = for {
+      file <- FileSystem(id)
+      val name = file.name
+      xs   <- FileSystem.read(file)
+    } yield (name,xs)
+    response match {
+      case Some((name, xs)) =>
+        Ok ~>
+          ResponseHeader("Content-Disposition", List("attachment; filename=\"%s\"".format(name))) ~>
+          CharContentType("application/octet-stream") ~>
+          ResponseBytes(xs)
+      case None =>
+        NotFound ~> Scalate(req, "404.scaml")
+    }
   case req =>
     NotFound ~> Scalate(req, "404.scaml")
 })
